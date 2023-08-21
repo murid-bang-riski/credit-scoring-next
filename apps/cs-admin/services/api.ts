b2e346511674ebccdc7d8285415846be0368f1fb
Zul Ham Fahlevi <zulham.fahlevi.73@gmail.com>
2023-08-21 16:57:36 +0700

    feat(login): migrasi UI login dari repositori sebelumnya

---
+++ apps/cs-admin/services/api.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+import type { AxiosRequestConfig } from "axios";
+import TokenService from "./token";
+
+const config: AxiosRequestConfig = {
+  baseURL: `import.meta.env.VITE_API_URL`,
+};
+
+const api = axios.create(config);
+
+api.interceptors.request.use(
+  async (config) => {
+    const token = TokenService.getToken();
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    Promise.reject(error);
+    TokenService.removeToken();
+  },
+);
+
+export default api;
