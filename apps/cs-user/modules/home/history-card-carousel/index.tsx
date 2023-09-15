9bfb853e6215f3591cc9a5193c161aa45c169ac1
dinda-ayu-syafitri <dinda-ayu-syafitri>
2023-09-15 19:23:20 +0700

    fix: Change history feature carousel from alice-carousel to swiper

--- apps/cs-user/modules/home/history-card-carousel/index.tsx
+++ apps/cs-user/modules/home/history-card-carousel/index.tsx
@@ -1,12 +1,12 @@
 import { Card } from "@cs-user/components";
-import { ReactElement } from "react";
-import AliceCarousel from "react-alice-carousel";
-import "react-alice-carousel/lib/alice-carousel.css";
+import { ReactElement, useState, useRef, useEffect } from "react";
 import { IconNext, IconPrev } from "@cs-user/components";
 import Image from "next/image";
-import { TFeatureHistoryItem } from "@cs-user/types";
-import { RiH1 } from "react-icons/ri";
 import { useCardData } from "./hooks";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
 
 export const HistoryCardCarousel = (data: any, isLoading: boolean): ReactElement => {
   const { getCardData } = useCardData();
@@ -25,78 +25,82 @@ export const HistoryCardCarousel = (data: any, isLoading: boolean): ReactElement
     };
   };
 
-  const cardProps = {
-    mouseTrackingEnabled: true,
-    disableDotsControls: true,
-    // item: <h1>test</h1>,
-    items: data?.data?.map((x: any, i: number) => {
-      const { icon, bgButton } = getIconAndBgButton(x.name); // Get icon and bgButton dynamically
-      return (
-        <div key={i} className="flex max-w-screen">
-          <Card
-            hasButton={true}
-            buttonText="Lihat Detail"
-            buttonHref="/dashboard/request?tab=hasil"
-            buttonClassName={`flex py-1 justify-center rounded-sm text-white items-end my-2 ${bgButton} w-full`}
-            className="w-[300px] m-3 min-h-[130px] absolute shadow-md px-4 py-3"
-          >
-            <div className="flex flex-row w-full h-full py-4 gap-2 justify-center items-center">
-              <div className="flex justify-center items-center w-corousel">
-                <Image
-                  src={icon}
-                  alt="icon"
-                  className="flex justify-center items-center"
-                  width={20}
-                  height={20}
-                />
-              </div>
+  const useSwiperRef = <T extends HTMLElement>(): [T | null, React.Ref<T>] => {
+    const [wrapper, setWrapper] = useState<T | null>(null);
+    const ref = useRef<T>(null);
 
-              <div className="flex-col w-full space-y-1 flex">
-                <p className="text-sm font-semibold pr-8">{x.name}</p>
-                <p className="text-neutral-400 font-normal text-xs">{`${x.total_data} permintaan`}</p>
-              </div>
-            </div>
-          </Card>
-        </div>
-      );
-    }),
-    responsive: {
-      0: {
-        items: 1,
-      },
-      600: {
-        items: 2,
-      },
-      1024: {
-        items: 4,
-        itemsFit: "contain",
-      },
-    },
-    autoPlay: true,
-    autoPlayInterval: 3000,
-    animationDuration: 1000,
-    Infinity: true,
-    renderPrevButton: () => {
-      return (
-        <div className="bg-white absolute -left-8 top-[35%] p-2 rounded-full shadow-md hover:bg-slate-100">
-          {<IconNext />}
-        </div>
-      );
-    },
-    renderNextButton: () => {
-      return (
-        <div className=" bg-white absolute -right-8 top-[35%] p-2 rounded-full shadow-md hover:bg-slate-100">
-          {<IconPrev />}
-        </div>
-      );
-    },
+    useEffect(() => {
+      if (ref.current) {
+        setWrapper(ref.current);
+      }
+    }, []);
+
+    return [wrapper, ref];
   };
+  const [nextEl, nextElRef] = useSwiperRef<HTMLDivElement>();
+  const [prevEl, prevElRef] = useSwiperRef<HTMLDivElement>();
 
   return (
     <div className="p-4 ">
       <h1 className="text-2xl font-bold uppercase p-3">Riwayat Permintaan</h1>
-      <div className="">
-        <AliceCarousel {...cardProps} />
+      <div className="flex justify-items-center align-middle items-center">
+        <div
+          className="bg-white p-2 rounded-full h-fit shadow-md hover:bg-slate-100 cursor-pointer"
+          ref={prevElRef}
+        >
+          {<IconNext />}
+        </div>
+        <Swiper
+          navigation={{
+            prevEl,
+            nextEl,
+          }}
+          modules={[Navigation]}
+          slidesPerView={4}
+          // spaceBetween={10}
+          // freeMode={true}
+        >
+          {data?.data?.map((x: any, i: number) => {
+            const { icon, bgButton } = getIconAndBgButton(x.name);
+            return (
+              <SwiperSlide key={i} className="flex">
+                <Card
+                  hasButton={true}
+                  buttonText="Lihat Detail"
+                  buttonHref="/dashboard/request?tab=hasil"
+                  buttonClassName={`flex py-1 justify-center rounded-sm text-white items-end my-2 ${bgButton} w-full`}
+                  bodyClassName="flex flex-col justify-center items-center w-full h-full"
+                  className="w-[300px] m-3 min-h-[130px] shadow-md px-4 py-3"
+                >
+                  <div className="flex flex-row w-full h-full py-4 gap-4 justify-center items-center">
+                    <div className="flex justify-center items-center">
+                      <Image
+                        src={icon}
+                        alt="icon"
+                        style={{ width: "80px", height: "80px" }}
+                        // className="flex justify-center items-center"
+                        width={0}
+                        height={0}
+                      />
+                    </div>
+
+                    <div className="flex-col w-full space-y-1 flex">
+                      <p className="text-md font-semibold pr-8">{x.name}</p>
+                      <p className="text-neutral-400 font-normal text-sm">{`${x.total_data} permintaan`}</p>
+                    </div>
+                  </div>
+                </Card>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+        <div
+          className=" bg-white h-fit p-2 rounded-full shadow-md hover:bg-slate-100 cursor-pointer"
+          ref={nextElRef}
+        >
+          {<IconPrev />}
+        </div>
+        {/* <AliceCarousel {...cardProps} /> */}
       </div>
     </div>
   );
